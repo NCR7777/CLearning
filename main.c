4b28f33d492fba8ff1bdebcd66983a56a477a2f2
nachunrui <ncr0119@163.com>
2021-04-09 21:48:31 +0800

    Chapter 2

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+void one_three();
+void two();
+
 int main(void) {
-    printf("Hello, World!\n");
-    getchar();
+    printf("Starting now\n");
+    one_three();
+    printf("Done!");
     return 0;
+}
+
+void one_three(){
+    printf("One\n");
+    two();
+    printf("Three\n");
+}
+
+void two(){
+    printf("Two\n");
 }
