6656e7b7d26fc54bd7ed0cf64818515008886aed
nachunrui <ncr0119@163.com>
2021-04-10 13:45:50 +0800

    Chapter 3 commit

--- chapter3.c
+++ chapter3.c
@@ -9,11 +9,11 @@ void chapter3() {
     // printf("十进制：%d，八进制：%o，十六进制：%x", x, x, x);
     // printf("\n");
     // printf("十进制：%d，八进制：%#o，十六进制：%#x", x, x, x);
+
     unsigned int un = 3000000000;
     short end = 200;
     long big = 65537;
     long long veryBig = 12345678908642;
-
     printf("un = %u and not %d\n", un, un);
     printf("end = %hd and %d\n", end, end);
     printf("big = %ld and not %hd\n", big, big);
